230d491a93453d10af0e2404fccbb04cb3e74677
BaGorK <edmealemkassahun@gmail.com>
2024-02-25 21:57:57 +0300

    Deleting a city from city list

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -53,7 +53,21 @@ function CitiesProvider({ children }) {
 
       setCities((cities) => [...cities, data]);
     } catch (err) {
-      console.error('There was an error fetching the data: ', err);
+      console.error('There was an error creating a city data: ', err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const deleteCity = async (id) => {
+    try {
+      setIsLoading(true);
+      await fetch(`${BASE_URL}/cities/${id}`, {
+        method: 'DELETE',
+      });
+
+      setCities((cities) => cities.filter((city) => city.id !== id));
+    } catch (err) {
+      console.error('There was an error deleting a city data: ', err);
     } finally {
       setIsLoading(false);
     }
@@ -61,7 +75,14 @@ function CitiesProvider({ children }) {
 
   return (
     <CitiesContext.Provider
-      value={{ cities, isLoading, currentCity, getCity, createCity }}
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        getCity,
+        createCity,
+        deleteCity,
+      }}
     >
       {children}
     </CitiesContext.Provider>
